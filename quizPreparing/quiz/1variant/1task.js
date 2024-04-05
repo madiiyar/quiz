66316761c6fbcc98008f4f56b587089d4a8ed136
madiiyar <amadiar654@gmail.com>
2024-04-05 13:53:39 +0500

    sd

--- quizPreparing/quiz/1variant/1task.js
+++ quizPreparing/quiz/1variant/1task.js
@@ -1,11 +1,13 @@
 const array = [1, 3, 5, 2, 5, 6, 7, 3, 6];
 
-function reverseArr(arr) {
-  newArr = [];
-  for (let i = arr.length - 1; i >= 0; i--) {
-    newArr.push(arr[i]);
-  }
-  console.log(newArr);
-}
+// function reverseArr(arr) {
+//   newArr = [];
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     newArr.push(arr[i]);
+//   }
+//   console.log(newArr);
+// }
 
-reverseArr(array);
+// reverseArr(array);
+
+console.log(array.toReversed());
